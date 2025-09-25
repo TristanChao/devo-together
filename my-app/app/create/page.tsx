c4fb2be70799f35d80ba44c7197db22979717228
Tristan Chao <45975980+TristanChao@users.noreply.github.com>
2025-09-25 14:39:16 -0700

    added conditional coloring to navbar icons to turn blue when on the corresponding page

--- my-app/app/create/page.tsx
+++ my-app/app/create/page.tsx
@@ -1,7 +1,11 @@
 export default function CreatePage() {
   return (
-    <div className="pt-20">
-      <input className="bg-[var(--color-accent)]" />
+    <div className="font-sans flex flex-col items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-start sm:items-start">
+        <input className="bg-[var(--color-accent)]" />
+
+        
+      </main>
     </div>
   )
 }
--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation"
+
 import { AiFillHome } from "react-icons/ai";
 import { FaCalendarAlt, FaSearch, FaHome } from "react-icons/fa"
 import { FaPenToSquare } from "react-icons/fa6";
@@ -7,19 +11,26 @@ import { IoPersonSharp } from "react-icons/io5";
 import { RiHomeFill } from "react-icons/ri";
 
 export default function Navbar() {
+  const currentPath = usePathname();
+
+  const mdIconClass = "w-7 h-7";
+  const lgIconClass = "w-10 h-10";
+
+  const blueIcon = " text-blue-500"
+  
   return (
     <div className="z-50 fixed w-full flex items-center py-3 bg-[var(--color-background)]">
       <div className="hidden lg:inline-block lg:basis-1/4">
         <Link href="/" className="text-xl ml-5 hidden sm:inline">DevoTogether</Link>
       </div>
-      
+
       <div className="basis-full lg:basis-1/2">
         <div className="justify-self-center flex justify-between items-center w-[280px] sm:w-[400px]">
-          <Link href="/daily"><FaCalendarAlt className="w-7 h-7"/></Link>
-          <Link href="/search"><FaSearch className="w-7 h-7"/></Link>
-          <Link href="/"><GoHomeFill className="w-10 h-10"/></Link>
-          <Link href="/create"><FaPenToSquare className="w-7 h-7"/></Link>
-          <Link href="/profile"><IoPersonSharp className="w-7 h-7"/></Link>
+          <Link href="/daily"><FaCalendarAlt className={currentPath === "/daily" ? mdIconClass+blueIcon : mdIconClass}/></Link>
+          <Link href="/search"><FaSearch className={currentPath === "/search" ? mdIconClass+blueIcon : mdIconClass}/></Link>
+          <Link href="/"><GoHomeFill className={currentPath === "/" ? lgIconClass+blueIcon : lgIconClass}/></Link>
+          <Link href="/create"><FaPenToSquare className={currentPath === "/create" ? mdIconClass+blueIcon : mdIconClass}/></Link>
+          <Link href="/profile"><IoPersonSharp className={currentPath === "/profile" ? mdIconClass+blueIcon : mdIconClass}/></Link>
         </div>
       </div>
     </div>
